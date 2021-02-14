a9ae70f1e7371bb7637876c1ec44490118548eca
Dimitri Keller <dmotroy@googlemail.com>
2021-02-14 17:13:26 +0100

    change dummy data in services

--- src/app/services/blog-post-comment.service.ts
+++ src/app/services/blog-post-comment.service.ts
@@ -8,9 +8,9 @@ import { MainService } from './main.service';
 export class BlogPostCommentService extends MainService {
   objects: BlogPostComment[] = [
     { id: 1, blog_post_id: 1, name: 'Max Mustermann', comment: 'Richtig guter Beitrag!', created_at: '2020-07-08' },
-    { id: 2, blog_post_id: 11, name: 'Max Mustermann', comment: 'Richtig guter Beitrag, weiter so!', created_at: '2020-08-27' },
-    { id: 3, blog_post_id: 11, name: 'Albert Einstein', comment: 'Selbst ich wusste es nicht!', created_at: '2020-08-27' },
-    { id: 4, blog_post_id: 11, name: 'Nicola Tesla', comment: 'Sehr große Spannung während des Lesens! Richtig gut!', created_at: '2020-08-27' },
+    { id: 2, blog_post_id: 5, name: 'Max Mustermann', comment: 'Richtig guter Beitrag, weiter so!', created_at: '2020-08-15' },
+    { id: 3, blog_post_id: 5, name: 'Albert Einstein', comment: 'Selbst ich wusste es nicht!', created_at: '2020-08-15' },
+    { id: 4, blog_post_id: 5, name: 'Nicola Tesla', comment: 'Sehr große Spannung während des Lesens! Richtig gut!', created_at: '2020-08-15' },
   ];
 
   constructor() { 
